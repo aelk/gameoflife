dfc7728f78a45e7fd5f432f5769720d7fb8432d8
Andrew Elkouri <andrewelkouri@gmail.com>
2020-05-17 17:18:46 -0400

    fixed bug with dead state

--- life.py
+++ life.py
@@ -1,7 +1,7 @@
 from random import randint
 
 def dead_state(width, height):
-    return [[0] * width] * height
+    return [[0 for i in range(width)] for j in range(height)]
 
 def random_state(width, height):
     state = dead_state(width, height)
@@ -10,10 +10,20 @@ def random_state(width, height):
             state[i][j] = randint(0, 1)
     return state
 
+def render(state):
+    for row in state:
+        print(row)
+    '''
+    for i in range(len(state)):
+        for j in range(len(state[0])):
+            if state[i][j] == 0:
+                print(" ")
+            else:
+                print("#")
+    '''
+
 if __name__ == '__main__':
-    width = 3
-    height = 5
-    print("width =", width)
-    print("height =", height)
+    width, height = 3, 5
     state = random_state(width, height)
     print(state)
+    render(state)
