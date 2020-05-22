dbb63d3beaa11de0dcb57921f193b26090d678f0
Andrew Elkouri <andrewelkouri@gmail.com>
2020-05-22 10:42:10 -0400

    count neighbors + tests

--- life.py
+++ life.py
@@ -20,12 +20,32 @@ def render(state):
         print('|')
     print("-" * border_width)
 
+def count_live_neighbors(state, pos):
+    x, y = pos
+    live_neighbors = 0
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if i < 0 or j < 0 or i >= len(state) or j >= len(state[0]):
+                continue
+            elif state[i][j] == 1 and (i, j) != pos:
+                live_neighbors += 1
+
+    return live_neighbors
+
 def next_board_state(state):
-    # TODO
+    # count_live_neighbors(state, pos) -> returns 0..8
+
+    # when a cell changes, append its pos to a list
+    # after you finish checking each cell, go through the list
+    # and toggle each of the positions
+    new_cell_positions = []
+
     return state
 
 if __name__ == '__main__':
-    width, height = 37, 28
+    width, height = 3, 2
     state = random_state(width, height)
     print(state)
-    render(state)
+    #render(state)
+    count_live_neighbors(state, (0, 1))
--- life_test.py
+++ life_test.py
@@ -1,7 +1,23 @@
 import unittest
-from life import next_board_state, dead_state
+from life import next_board_state, dead_state, count_live_neighbors
 
 class LifeTest(unittest.TestCase):
+    def test_no_neighbors(self):
+        width, height = 2, 3
+        state = dead_state(width, height)
+        for i in range(width):
+            for j in range(height):
+                actual = count_live_neighbors(state, (i, j))
+                self.assertEqual(0, actual)
+
+    def test_one_neighbor(self):
+        state = [
+            [0, 0, 1],
+            [1, 1, 0]
+        ]
+        actual = count_live_neighbors(state, (0, 2))
+        self.assertEqual(1, actual)
+
     def test_next_empty(self):
         expected = dead_state(3, 4)
         actual = next_board_state(expected)
@@ -35,12 +51,12 @@ class LifeTest(unittest.TestCase):
         initial_state = [
             [1, 0, 1],
             [1, 1, 0],
-            [0, 0, 0],
+            [0, 0, 1],
         ]
         expected_state = [
-            [1, 1, 1],
             [1, 0, 0],
-            [0, 0, 0],
+            [1, 0, 1],
+            [0, 1, 0],
         ]
         actual_state = next_board_state(initial_state)
         self.assertEqual(expected_state, actual_state)
